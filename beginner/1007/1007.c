ec6143ca7ea42b6adf3db0242504c6ae943ffd8d
suicinxv <77497869+suicinxv@users.noreply.github.com>
2025-07-17 01:01:49 +0000

    1007 - C

---
+++ beginner/1007/1007.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(){
+    int a,b,c,d;
+
+    scanf("%i", &a);
+    scanf("%i", &b);
+    scanf("%i", &c);
+    scanf("%i", &d);
+
+    printf("DIFERENCA = %i\n", ((a*b)-(c*d)) );
+
+}
