fe3f5c087aee7351f347cab04cd0dca34a1bf1b0
suicinxv <77497869+suicinxv@users.noreply.github.com>
2026-01-02 22:43:59 +0000

    1035 - C

---
+++ beginner/1035/1035.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(){
+    int a, b, c , d;
+
+    scanf("%i", &a);
+    scanf("%i", &b);
+    scanf("%i", &c);
+    scanf("%i", &d);
+
+    if (b>c && d>a && (c+d) > (a+b) && c >=0 && d >=0 && (a%2) == 0)
+    {
+        printf("Valores aceitos\n");
+    }else{
+        printf("Valores nao aceitos\n");
+    }
+    
+}
