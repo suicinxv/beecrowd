33e4ddb4d8865f1edea74223d8ad4ad55d380002
suicinxv <77497869+suicinxv@users.noreply.github.com>
2026-01-04 19:36:07 +0000

    1041 - C

---
+++ beginner/1041/1041.c
@@ -0,0 +1,40 @@
+# include <stdio.h>
+
+int main(){
+
+    float x,y;
+
+    scanf("%f %f", &x, &y);
+
+    if (x == 0 && y != 0)
+    {
+        printf("Eixo Y\n");
+    }
+    if (y == 0 && x != 0)
+    {
+        printf("Eixo X\n");
+    }
+    
+    if (x > 0 && y != 0)
+    {
+        if(y>0){
+            printf("Q1\n");
+        }
+        if(y<0){
+            printf("Q4\n");
+        }
+    }
+    if( x < 0 && y != 0){
+        if(y>0){
+            printf("Q2\n");
+        }else{
+            printf("Q3\n");
+        }
+    }
+    if (x == 0 && y == 0)
+    {
+        printf("Origem\n");
+    }
+    
+    
+}
