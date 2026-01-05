fc90ca25a2557a2eeb3d4c83967f29c3bc3d96b0
suicinxv <77497869+suicinxv@users.noreply.github.com>
2026-01-05 20:39:23 +0000

    1008 - Python

---
+++ beginner/1008/1008.py
@@ -0,0 +1,7 @@
+number = int(input())
+worked_hours = int(input())
+amount_hour = float(input())
+salary = worked_hours*amount_hour
+
+print(f"NUMBER = {number}")
+print(f"SALARY = U$ {salary:.2f}")
