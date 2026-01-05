2cc10f91cb74e961aa69773c59aac9f2b535eee0
suicinxv <77497869+suicinxv@users.noreply.github.com>
2026-01-05 01:48:10 +0000

    1005 - C++

---
+++ beginner/1005/1005.cpp
@@ -0,0 +1,13 @@
+# include <iostream>
+# include <iomanip>
+
+int main(){
+    double a, b, media;
+
+    std::cin >> a;
+    std::cin >> b;
+
+    media = ((a*3.5)+(b*7.5)) / (3.5+7.5);
+
+    std::cout << "MEDIA = " << std::fixed << std::setprecision(5) << media << "\n";
+}
